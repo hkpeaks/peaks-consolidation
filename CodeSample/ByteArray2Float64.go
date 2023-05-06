a0cad314bcd5aa23ceb9be0202b3f6ff087e2ffb
Max Yu <130877963+hkpeaks@users.noreply.github.com>
2023-05-06 20:58:30 +0800

    Update ByteArray2Float64.go

--- CodeSample/ByteArray2Float64.go
+++ CodeSample/ByteArray2Float64.go
@@ -1,30 +1,34 @@
-// This code is use to convert bytearray to float64 directly
-
 // Traditional indirect method: Convert to string and then convert to float64
 // float_number, err = strconv.ParseFloat(string(bytestream[start_byte:end_byte]), 64)
 
-// Usage
+// This code directly converts a bytearray to float64.
 // float_number = ByteArray2Float64(bytestream[start_byte:end_byte])
 
 func ByteArray2Float64(current_cell []byte) float64 {
-	var multiply10Pow, divide10Pow, digit, offset, current_byte, left_byte int
-	var total_amount float64
-	var is_dot_exist, is_integer_complete, is_negative, is_invalid_number bool
-	is_dot_exist, is_integer_complete, is_negative, is_invalid_number = false, false, false, false
-	total_amount, multiply10Pow, divide10Pow = 0, 0, 0
 
-	var start_byte, total_byte = 0, len(current_cell)
+	var float_number float64	
+	var multiply10Pow, divide10Pow, position, offset, current_byte, left_byte int	
+	var is_dot_exist, is_integer_complete, is_negative, is_invalid_number  = false, false, false, false
+	
+	var total_byte = len(current_cell)
 
 	for current_byte < total_byte {
-
-		if current_cell[current_byte] == 40 || current_cell[current_byte] == 41 || current_cell[current_byte] == 45 {
-			current_cell[current_byte] = 48
-			is_negative = true
-		} else if current_cell[current_byte] == 46 {
+		switch current_cell[current_byte] {
+		case '.':
 			is_dot_exist = true
-		} else if current_cell[current_byte] < 48 || current_cell[current_byte] > 57 {
-			is_invalid_number = true
-		}
+		case '-':
+			current_cell[current_byte] = '0'
+			is_negative = true
+		case '(':
+			fallthrough
+		case ')':
+			fallthrough
+		
+		default:
+			if current_cell[current_byte] < 48 || current_cell[current_byte] > 57 {
+				is_invalid_number = true
+			}
+		}		
 
 		if !is_integer_complete {
 			if is_dot_exist || (!is_dot_exist && current_byte == total_byte-1) {
@@ -32,52 +36,51 @@ func ByteArray2Float64(current_cell []byte) float64 {
 					multiply10Pow++
 				}
 				offset = 0
-				for left_byte < start_byte+multiply10Pow {
-					current_digit := int((current_cell[left_byte] - 48))
-					digit = 0
-					for digit+offset < multiply10Pow-1 {
+				for left_byte < multiply10Pow {
+					current_digit := float64(current_cell[left_byte] - 48)
+					position = 0
+					for position+offset < multiply10Pow-1 {
 						current_digit *= 10
-						digit++
+						position++
 					}
 					offset++
 					left_byte++
-					total_amount += float64(current_digit)
+					float_number += current_digit
 				}
 
 				is_integer_complete = true
+				divide10Pow++
+				
 			}
+			multiply10Pow++
+
 		} else if is_dot_exist {
 			if current_byte == total_byte-1 {
 				offset = 0
 
 				for right_byte := total_byte - 1; right_byte >= total_byte-divide10Pow; right_byte-- {
-					current_digit := float64((current_cell[right_byte] - 48)) * 0.1
-					digit = 0
-					for digit+offset < divide10Pow-1 {
+					current_digit := float64(current_cell[right_byte] - 48) * 0.1
+					position = 0
+					for position+offset < divide10Pow-1 {
 						current_digit *= 0.1
-						digit++
+						position++
 					}
 					offset++
-					total_amount += float64(current_digit)
+					float_number += current_digit
 				}
 			}
-		}
-		if is_integer_complete {
 			divide10Pow++
-		} else {
-			multiply10Pow++
-		}
+		}		
 
 		current_byte++
 	}
 
 	if is_negative == true {
-		total_amount *= -1
+		float_number *= -1
 	}
 
 	if is_invalid_number == true {
-		total_amount = 0
+		float_number = 0
 	}
-
-	return total_amount
+	return float_number
 }
