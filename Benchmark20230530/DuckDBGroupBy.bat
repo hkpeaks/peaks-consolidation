774ab4a5a147d651fb118832099e85bcfc6d8236
Max Yu <130877963+hkpeaks@users.noreply.github.com>
2023-05-31 08:38:18 +0800

    Add files via upload

---
+++ Benchmark20230530/DuckDBGroupBy.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set start_time=!time!
+
+:: SQL Statement
+duckdb -c "copy (SELECT Ledger, Account, DC, Currency, SUM(Base_Amount) as Total_Base_Amount FROM read_csv_auto('input/300-MillionRows.csv') GROUP BY Ledger, Account, DC, Currency) to 'output/DuckGroupBy.csv' (format csv, header true);"
+
+
+set end_time=!time!
+
+echo Start time: %start_time%
+echo End time: %end_time%
+
+:: Calculate elapsed time
+set /a hours=%end_time:~0,2%-%start_time:~0,2%
+set /a minutes=%end_time:~3,2%-%start_time:~3,2%
+set /a seconds=%end_time:~6,2%-%start_time:~6,2%
+
+:: Output elapsed time in seconds
+set /a elapsed_seconds=hours*3600+minutes*60+seconds
+echo Elapsed time in seconds: %elapsed_seconds%
