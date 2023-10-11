4120112256fbe86fac8f37181746002562d899a8
Max Yu <130877963+hkpeaks@users.noreply.github.com>
2023-10-12 01:08:56 +0800

    Add files via upload

---
+++ NewPeaksApp/1000Jobs/concurrentuser.py
@@ -0,0 +1,29 @@
+import subprocess
+import concurrent.futures
+import time
+
+start_time = time.time()
+
+# specify the script you want to run
+script = "peakrs.py"
+
+# specify the argument you want to pass to the script
+argument = "1M_Fact.csv"
+
+def run_script(username):
+    # create a new process to run the script with the argument and username
+    process = subprocess.Popen(["python", script, argument, username])
+
+    # wait for the process to complete
+    process.wait()
+
+# create a list of unique usernames
+usernames = [f"user{i}" for i in range(1, 1001)]
+
+# create a ThreadPoolExecutor
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    # use map to run the function in parallel for each username
+    executor.map(run_script, usernames)
+
+end_time = time.time()
+print("Polars Duration (In Second): {}".format(round(end_time-start_time,3)))
